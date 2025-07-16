64d553e8f057100a6a46561de508f957d4616e77
slaven <slaven.stanic@based-technology.hr>
2025-07-16 14:04:20 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Typography } from "@mui/material";
+import type { ReactElement } from "react";
+
+export const App = (): ReactElement => {
+	return (
+		<div>
+			<Typography>Initial Commit</Typography>
+		</div>
+	);
+};
