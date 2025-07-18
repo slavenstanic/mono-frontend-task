6fe9695f2d77a9fe34addb95da873d1b79218bae
slaven <slaven.stanic@based-technology.hr>
2025-07-18 10:58:25 +0200

    updated styling

--- src/components/hero/HeroSection.tsx
+++ src/components/hero/HeroSection.tsx
@@ -3,6 +3,8 @@ import { Filters } from "@/components/filters/Filters.tsx";
 import { AdGrid } from "@/components/grid/AdGrid.tsx";
 
 const Root = styled("div")(() => ({
+	width: "100%",
+	maxWidth: "73.9425rem",
 	display: "flex",
 	alignItems: "flex-start",
 	gap: "2rem",
