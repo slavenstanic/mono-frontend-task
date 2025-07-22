58ceb532236855ee62df01a563ad953d05dd7447
slaven <slaven.stanic@based-technology.hr>
2025-07-22 18:55:08 +0200

    implemented router into app

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,17 @@
 import type { ReactElement } from "react";
-import { useState } from "react";
-import type { AdProps } from "@/api/hooks/fetchVehicles.ts";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { AddVehiclePage } from "@/pages/AddVehiclePage.tsx";
 import { EditVehiclePage } from "@/pages/EditVehiclePage.tsx";
 import { MainPage } from "@/pages/MainPage.tsx";
 
 export const App = (): ReactElement => {
-	const [selectedVehicle, setSelectedVehicle] = useState<AdProps | null>(null);
-	const [isEditing, setIsEditing] = useState(false);
-
 	return (
-		<div>
-			{isEditing && selectedVehicle ? (
-				<EditVehiclePage
-					vehicle={selectedVehicle}
-					onClose={() => setIsEditing(false)}
-				/>
-			) : (
-				<>
-					<MainPage
-						onEdit={(vehicle) => {
-							setSelectedVehicle(vehicle);
-							setIsEditing(true);
-						}}
-					/>
-					<AddVehiclePage />
-				</>
-			)}
-		</div>
+		<Router>
+			<Routes>
+				<Route path="/" element={<MainPage />} />
+				<Route path="/add" element={<AddVehiclePage />} />
+				<Route path="/edit/:id" element={<EditVehiclePage />} />
+			</Routes>
+		</Router>
 	);
 };
